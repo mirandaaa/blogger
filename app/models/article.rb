eede39fcb8d5d5966e937ac90144568fcfe4e097
Miranda Wang <miranda.wang@MIRANDAWANG.local>
2016-07-25 16:47:12 -0400

    Tagging feature completed

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,3 +1,15 @@
 class Article < ApplicationRecord
   has_many :comments
+  has_many :taggings
+  has_many :tags, through: :taggings
+
+  def tag_list
+    self.tags.collect(&:name).join(',')
+  end
+
+  def tag_list=(tags_string)
+    tags = tags_string.split(",").each{ |s| s.strip.downcase }.uniq
+    new_or_found_tags = tags.collect { |name| Tag.find_or_create_by(name: name) }
+    self.tags = new_or_found_tags
+  end
 end
