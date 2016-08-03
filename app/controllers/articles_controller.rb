bcaa5521d173ca252f74bbef32f52a6139638c35
Miranda Wang <miranda.wang@MIRANDAWANG.local>
2016-08-03 13:38:02 -0400

    Implementing helper method to access article variable without using instance variables

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,7 +2,12 @@ class ArticlesController < ApplicationController
   include ArticlesHelper
 
   before_filter :require_login, except: [:index, :show]
-  helper_method :articles_by_month
+  helper_method :articles_by_month, :article
+
+  def article
+    @cached_article ||= Article.find_or_initialize_by(id: params[:id])
+  end
+
 
   def index
     @articles = Article.all
@@ -13,38 +18,33 @@ class ArticlesController < ApplicationController
   end
 
   def show
-    @article = Article.find(params[:id])
     @comment = Comment.new
-    @comment.article_id = @article.id
+    @comment.article_id = article.id
   end
 
   def new
-    @article = Article.new
   end
 
   def create
-    @article = Article.new(article_params)
-    @article.save
-    flash.notice = "Article '#{@article.title}' Created!"
-    redirect_to article_path(@article)
+    article.attributes = article_params
+    article.save
+    flash.notice = "Article '#{article.title}' Created!"
+    redirect_to article_path(article)
   end
 
   def destroy
-    @article = Article.find(params[:id])
-    @article.destroy
-    flash.notice = "Article '#{@article.title}' Deleted!"
+    article.destroy
+    flash.notice = "Article '#{article.title}' Deleted!"
     redirect_to articles_path
   end
 
   def edit
-    @article = Article.find(params[:id])
   end
 
   def update
-    @article = Article.find(params[:id])
-    @article.update(article_params)
-    flash.notice = "Article '#{@article.title}' Updated!"
-    redirect_to article_path(@article)
+    article.update(article_params)
+    flash.notice = "Article '#{article.title}' Updated!"
+    redirect_to article_path(article)
   end
 
 end
